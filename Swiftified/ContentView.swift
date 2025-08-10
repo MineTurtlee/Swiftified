f3cbc6f101a6d60bf02dde78d1db92794fd32249
MineTurtlee <97010924+MineTurtlee@users.noreply.github.com>
2025-08-10 18:13:26 +0700

    Initial Commit

---
+++ Swiftified/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiftified
+//
+//  Created by GreenyCells (Mineturtlee) on 10/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiftified/SwiftifiedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftifiedApp.swift
+//  Swiftified
+//
+//  Created by GreenyCells (Mineturtlee) on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftifiedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
