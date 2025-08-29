d742649aac8d91e864801e008587a07cbc30f680
MineTurtlee <97010924+MineTurtlee@users.noreply.github.com>
2025-08-29 21:04:09 +0700

    iOS App finally it work.

--- Swiftified/ContentView.swift
+++ Swiftified/ContentView.swift
@@ -4,23 +4,32 @@
 //
 //  Created by GreenyCells (Mineturtlee) on 10/8/25.
 //
-// MTM5OTUwNDU4NDQzMDQ1Mjc4Ng.GkYZ5Q.2lhQIhYYoGuluoyJ8UEisg3SKP6JoFFZ8pKmmo
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationSplitView {
-            VStack {
+        #if os(macOS)
+            NavigationSplitView {
+                VStack {
+                    List {
+                        NavigationLink("Main", destination: Main())
+                        NavigationLink("Settings", destination: Settings())
+                    }
+                }
+            }
+            detail: {
+                Main()
+            }
+        #elseif os(iOS)
+            NavigationStack {
                 List {
                     NavigationLink("Main", destination: Main())
                     NavigationLink("Settings", destination: Settings())
                 }
             }
-        }
-        detail: {
-            Main()
-        }
+            .navigationTitle("Swiftified")
+        #endif
     }
 }
 
