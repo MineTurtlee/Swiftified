102502bd7d8b09bc5e472756d4f0adb83644856d
Mineturtlee <97010924+MineTurtlee@users.noreply.github.com>
2026-03-25 19:27:53 +0700

    deflood

--- Swiftified/Sources/Swiftified/Commands/Base.swift
+++ Swiftified/Sources/Swiftified/Commands/Base.swift
@@ -11,8 +11,28 @@ struct Ping: Command {
     static let name = "ping"
     static let description = "Ping pong :)"
 
-    static func handleSlash(_ interaction: DiscordInteraction) async {
-        //
+    static func handleSlash(_ client: DiscordClient, interaction: DiscordInteraction) async {
+        var latencies = try! await ping()
+        interaction.editInteraction(
+            client: client,
+            interaction: interaction, 
+            data: DiscordMessage.Edit(
+                components: [
+                    DiscordMessageComponent.container(
+                        components: [
+                            DiscordMessageComponent.textDisplay(content: "# Ping statistics"),
+                            DiscordMessageComponent.separator(),
+                            DiscordMessageComponent.textDisplay(content: 
+                                """
+                                API latency: \(latencies[0])
+                                Websocket latency: \(latencies[1])
+                                """
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
     }
     
     static func handleMessage(_ message: DiscordMessage, _ args: [String]) async {
---
+++ Swiftified/Sources/Swiftified/Stuff/Ping.swift
@@ -0,0 +1,42 @@
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+import Discord
+import Logging
+
+fileprivate let logger = Logger(label: "SwiftifiedStuff")
+
+func ping() async throws -> [Int] {
+    guard let apiURL = URL(string: "https://discord.com/api/v10/gateway") else {
+        logger.error("Invalid API URL.")
+        return [0, 0]
+    }
+
+    guard let wsURL = URL(string: "https://gateway.discord.gg/?v=10&encoding=json") else {
+        logger.error("Invalid WS URL.")
+        return [0, 0]
+    }
+    
+    let config = URLSessionConfiguration.default
+    config.requestCachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+    let session = URLSession(configuration: config)
+
+    let apiStart = DispatchTime.now()
+    let (_, _) = try await session.data(from: apiURL)
+    let apiLatency = Int((DispatchTime.now().uptimeNanoseconds - apiStart.uptimeNanoseconds) / 1_000_000)
+
+    let wsStart = DispatchTime.now()
+    let (_, _) = try await session.data(from: wsURL)
+    let wsLatency = Int((DispatchTime.now().uptimeNanoseconds - wsStart.uptimeNanoseconds) / 1_000_000)
+
+    return [apiLatency, wsLatency]
+}
+
+func noPerms(userID: UserID, command: String, prefix: String) -> String {
+    return "You are not allowed to use this command!\n-# Replied to <@\(userID)> • Command: \(prefix)\(command)"
+}
+
+func parseGuild(guildId: GuildID, client: DiscordClient) -> DiscordGuild {
+    return client.guilds[guildId]!
+}
